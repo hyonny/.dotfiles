ee150779eb6c60b311c14c6b04859a10ff260048
hyonny <qooccom@gmail.com>
2019-11-26 14:23:15 +0900

    :+1: Clean ファイル名に"."を付ける必要がないので削除した

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -10,11 +10,11 @@ unlink ~/.commit_template
 
 echo "Link dotfiles to home directory from .dotfiles directory."
 
-ln -s ~/.dotfiles/bash/.bashrc ~/.bashrc
-ln -s ~/.dotfiles/bash/.bash_profile ~/.bash_profile
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/git/.git-prompt.sh ~/.git-prompt.sh
-ln -s ~/.dotfiles/git/.commit_template ~/.commit_template
+ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.dotfiles/git/commit_template ~/.commit_template
 
 echo "Finished."
 
