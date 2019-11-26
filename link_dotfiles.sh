d98699df4cc8e9fde2580e64525b49bc7f7a945c
hyonny <qooccom@gmail.com>
2019-11-26 18:50:12 +0900

    :tada: Add vimrc

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -7,6 +7,7 @@ unlink ~/.bash_profile
 unlink ~/.gitconfig
 unlink ~/.git-prompt.sh
 unlink ~/.commit_template
+unlink ~/.vimrc
 
 echo "Link dotfiles to home directory from .dotfiles directory."
 
@@ -15,6 +16,7 @@ ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/git-prompt.sh ~/.git-prompt.sh
 ln -s ~/.dotfiles/git/commit_template ~/.commit_template
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 
 echo "Finished."
 
